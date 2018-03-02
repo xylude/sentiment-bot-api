bd001e6c832693dd83675571744c1cba6d13e4e3
Jason Crider <xylude@gmail.com>
2018-03-02 11:45:02 -0500

    fixing access control bullshit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import users from './routes/users'
 import subreddits from './routes/subreddits'
 import reddit from './routes/reddit'
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, jwt')
+	if ('OPTIONS' === req.method) {
+		return res.sendStatus(200);
+	}
+	next()
+})
+
 app.get('/users', users);
 app.get('/subreddits', subreddits);
 app.get('/reddit', reddit);
