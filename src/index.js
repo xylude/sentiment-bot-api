e284582d8aca1b289273b7ca631545fcf606744b
Jason Crider <xylude@gmail.com>
2018-03-02 11:29:12 -0500

    asdf

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import express from 'express'
 const app = express()
 
 import users from './routes/users'
+import subreddits from './routes/subreddits'
+import reddit from './routes/reddit'
 
-app.get('/users', users)
+app.get('/users', users);
+app.get('/subreddits', subreddits);
+app.get('/reddit', reddit);
 
 app.listen(3300, () => console.log('Example app listening on port 3300!'))
